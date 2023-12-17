cfadb689843d1804481176cb2ed7418e71379ebe
Miaseodov Tony <myasoedov.blog@gmail.com>
2023-12-17 16:44:27 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import Inspect from 'vite-plugin-inspect'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), Inspect()],
+    css: {
+        devSourcemap: true
+    },
+    resolve: {
+        alias: {
+            '@': '/src'
+        }
+    }
+})
